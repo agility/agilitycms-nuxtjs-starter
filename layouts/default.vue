99ab7991e1b4460e22b11c080152d269503697b2
Joel Varty <joel@agilitycms.com>
2021-03-01 10:13:47 -0500

    added preview bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,18 @@
 <template>
 	<div>
-		<GlobalHeader />
+		<preview-bar/>
+		<global-header/>
 	<Nuxt />
 	</div>
 </template>
 
 <script>
 import GlobalHeader from '../src/components/GlobalHeader.vue';
+import PreviewBar from '../src/components/PreviewBar.vue';
 export default {
 	components: {
-		GlobalHeader
+		GlobalHeader,
+		PreviewBar
 	}
 
 }
